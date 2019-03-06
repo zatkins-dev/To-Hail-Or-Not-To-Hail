f96b341e2f504cdd01887828c92cac72a715d590
Gage Phillips <gagerdude@gmail.com>
2019-03-06 17:55:14 -0600

    Pulls an entire table, cleans it, and then writes it to a file

---
+++ tableToCSV.py
@@ -0,0 +1,75 @@
+from google.cloud import bigquery
+
+import csv
+import os.path
+
+ALL_COLS = ("stn", "wban", "year", "mo", "da", "temp", "count_temp", "dewp", 
+    "count_dewp", "slp", "count_slp", "stp", "cound_stp", "visib", "count_visib",
+    "wdsp", "count_wdsp", "mxpst", "gust", "max", "flag_max", "min", "flag_min",
+    "prcp", "flag_prcp", "sndp", "fog", "rain_drizzle", "snow_ice_pellets",
+    "hail", "thunder", "tornado_funnel_cloud")
+
+def get_full_table(year):
+    client = bigquery.Client()
+    gsod_dataset_ref = client.dataset('noaa_gsod', project='bigquery-public-data')
+    gsod_dset = client.get_dataset(gsod_dataset_ref)
+    gsod_full = client.get_table(gsod_dset.table(f'gsod{year}'))
+
+    return  client.list_rows(gsod_full)
+    
+def is_bad_row(row, schema):
+    bad_dict = {
+        "temp": 9999.9,
+        "dewp": 9999.9,
+        "slp":  9999.9,
+        "stp":  9999.9,
+        "visib": 999.9,
+        "wdsp": 999.9,
+        "mxpsp": 999.9,
+        "gust": 999.9,
+        "max": 9999.9,
+        "min": 9999.9,
+        "prcp": 99.99,  # prcp and sndp may need a deeper look than just this
+        "sndp": 999.9,
+    }
+
+    for col in schema:
+        if col.name in bad_dict and row[col.name] == bad_dict[col.name]:
+            return True
+
+    return False
+
+def clean_table(table):
+    return [row for row in table if not is_bad_row(row, table.schema)]
+
+
+def table_to_csv(year):
+    # quick check to prevent duplicate requests
+    if os.path.exists(f'csv/gsod{year}.csv'):
+        print("File for this year already exists")
+        return
+
+    # first, get the data
+    print(f'Getting data for {year}')
+    table = clean_table(get_full_table(year))
+
+    # check if any clean data was actually found
+    if len(table) == 0:
+        # if none was found, jump ship
+        print(f'No clean data was found for {year}. Exiting...')
+        return
+    else:
+        print("Data retrieved! Writing to file...")
+
+    # create a csv file, and a writer to fill it
+    csv_file = open(f'csv/gsod{year}.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+
+    # fill the table using the data retrieved
+    csv_writer.writerows(table)
+
+    print("Done!")
+
+    # close the file
+    csv_file.close()
+
