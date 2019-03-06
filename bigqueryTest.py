cf504191295994a7a4bcc2871af8eeb83e73a522
Zachary Atkins <zacharyjayhawk@gmail.com>
2019-03-05 20:04:55 -0600

    updated test code

--- bigqueryTest.py
+++ bigqueryTest.py
@@ -2,13 +2,14 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-query = client.query("""
-    SELECT *
-    FROM `bigquery-public-data.noaa_gsod.stations`
-    WHERE stn=30050 AND year=1929 AND mo=10
-""")
+gsod_dataset_ref = client.dataset('noaa_gsod', project='bigquery-public-data')
 
-results=query.results()
+gsod_dset = client.get_dataset(gsod_dataset_ref)
+
+gsod_full = client.get_table(gsod_dset.table('gsod1929'))
+
+schema_subset = [col for col in gsod_full.schema if col.name in ("stn","year","mo","da","temp", "dewp", "slp", "hail")]
+results = [x for x in client.list_rows(gsod_full, start_index=100, selected_fields=schema_subset, max_results=10)]
 
 print("stn","year","mo","da","temp", "dewp", "slp", "hail")
 for row in results:
