d67f616b97cdf435b86610a7a5b19cd178f550d1
Zachary Atkins <zacharyjayhawk@gmail.com>
2019-03-05 20:55:02 -0600

    added hail/ no hail counts

--- bigqueryTest.py
+++ bigqueryTest.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from functools import reduce
 
 client = bigquery.Client()
 
@@ -12,6 +13,11 @@ schema_subset = [col for col in gsod_full.schema if col.name in ("stn","year","m
 results = [x for x in client.list_rows(gsod_full, start_index=0, selected_fields=schema_subset, max_results=1000) 
                    if x.temp != 9999.9 and x.dewp != 9999.9 and x.slp != 9999.9 and x.stp != 9999.9 and x.wdsp != 999.9]
 
+num_results = len(results)
+num_hail = reduce(lambda acc,x2: acc + int(x2.hail), results,0)
+num_nohail = len(results) - num_hail
+
+print(f"Entries (No Hail/Hail): {num_results} ({num_nohail}/{num_hail})")
 print("stn","year","mo","da","temp", "dewp", "slp", "stp", "wdsp", "hail")
 for row in results:
     print(row.stn, row.year, row.mo, row.da, row.temp, row.dewp, row.slp, row.stp, row.wdsp, row.hail)
