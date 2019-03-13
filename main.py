64e5f5e640cc499f84dcccfd1053d1404a21bf95
Zachary Atkins <zacharyjayhawk@gmail.com>
2019-03-13 01:52:33 -0500

    Edited schema maker

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-gsod_dataset_ref = client.dataset('noaa_gsod', project='bigquery-public-data')
+gsod_dataset_ref = client.dataset('gsod_copy', project='to-hail-or-not-to-hail')
 
 gsod_dset = client.get_dataset(gsod_dataset_ref)
 
-gsod_full = client.get_table(gsod_dset.table('gsod2015'))
+gsod_full = client.get_table(gsod_dset.table('data'))
 
 schema = ""
 
 for scheme in gsod_full.schema:
-    schema += (f"{scheme.name}:{scheme.field_type},")
+    schema += (f"{scheme.name},")# {scheme.field_type},")
+
+schema = schema.replace("FLOAT", "FLOAT64")
+schema = schema.replace("INTEGER", "INT64")
+
+
 
 print (schema)
