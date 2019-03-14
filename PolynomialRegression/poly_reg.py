498dc97948411cb6fee9c274b81161b0e99e7ed0
Zachary Atkins <zacharyjayhawk@gmail.com>
2019-03-13 23:05:13 -0500

    Improved persistance for poly_reg model

--- PolynomialRegression/poly_reg.py
+++ PolynomialRegression/poly_reg.py
@@ -10,38 +10,15 @@ from json import dumps
 import datetime
 import os
 import sys
-
+from functools import reduce
+import easygui 
 
 class Model():
-    def __init__(self, model_id='poly_model', model_ext='.joblib', model_compress=0):
-        self._id = model_id
-        self._ext = model_ext
-        self.update_paths()
-        self._compress = model_compress
+    def __init__(self):
         self._target = None
         self._features = None
         self._poly_features = None
         self._model = None
-        model_loaded = False
-        while not model_loaded:
-            if os.path.exists(self.path_meta):
-                old_model_meta = load(self.path_meta)
-                print(old_model_meta)
-
-                self._target = set(old_model_meta['target'])
-                self._features = set(old_model_meta['features'])
-                self._compress = old_model_meta['compress']
-                self._degree = old_model_meta['degree']
-                self._model = load(self.path)
-                self._poly_features = load(self.path_poly_features)
-
-                model_loaded = True
-            else:
-                model_loaded = True
-
-    def __del__(self):
-        del self._poly_features
-        del self._model
 
     def train(self, target, train_data, degree=4):
         if self._model is not None:
@@ -54,42 +31,8 @@ class Model():
         self._target = set([target])
         self._features = train_data.columns - self._target
         self._degree = degree
-
-        model_loaded = False
-        while not model_loaded:
-            if os.path.exists(self.path_meta):
-                old_model_meta = load(self.path_meta)
-                if set(old_model_meta['features']) != self.features or set(old_model_meta['target']) != self.target or old_model_meta['degree']!=self._degree:
-                    conflict_resolved = False
-                    while not conflict_resolved:
-                        print(
-                            "Existing model with conflicting features or target '" + self.id + "' exists.")
-                        result_overwrite = ''
-                        while result_overwrite != 'y' and result_overwrite != 'n':
-                            result_overwrite = input(
-                                "Overwrite? (Y/n) > ").lower()
-                        if result_overwrite == 'y':
-                            os.remove(self.path_meta)
-                            os.remove(self.path)
-                            os.remove(self.path_poly_features)
-                            conflict_resolved = True
-                        else:
-                            result_rename = ''
-                            while result_rename != 'y' and result_rename != 'n':
-                                result_rename = input(
-                                    "Give new model id? (Y/n) > ").lower()
-                            if result_rename == 'y':
-                                self.id = input("New model id: > ")
-                            else:
-                                raise Exception(
-                                    "Model creation failed - conflicting '"+self.id+"' already exists.")
-                else:
-                    conflict_resolved = True
-                    model_loaded = True
-            else:
-                model_loaded = True
-
         self._poly_features = PolynomialFeatures(degree=self._degree)
+
         data_poly = self._poly_features.fit_transform(
             train_data.data.loc[:, self.features])
 
@@ -102,7 +45,7 @@ class Model():
             train_data.data.loc[:, self.target], target_predicted))
         r2 = r2_score(train_data.data.loc[:, self.target], target_predicted)
 
-        return {'rmse':rmse, 'r2':r2}
+        return {'RMSE':rmse, 'R-Squared':r2}
 
     def test(self, test_data):
         test_poly = self._poly_features.transform(
@@ -113,77 +56,7 @@ class Model():
             test_data.data.loc[:, self.target], test_predicted))
         r2 = r2_score(test_data.data.loc[:, self.target], test_predicted)
 
-        return {'rmse':rmse, 'r2':r2}
-
-    def dump(self):
-        if self.meta is None:
-            return False
-
-        dump(self.meta, self._path_meta)
-        dump(self._model, self._path, compress=self._compress)
-        dump(self._poly_features, self._path_poly_features, compress=self._compress)
-        return True
-
-    @property
-    def path(self):
-        if not self._path:
-            self.update_paths()
-        return self._path
-
-    @property
-    def path_poly_features(self):
-        if not self._path_poly_features:
-            self.update_paths()
-        return self._path_poly_features
-
-    @property
-    def path_meta(self):
-        if not self._path_meta:
-            self.update_paths()
-        return self._path_meta
-
-    @property
-    def meta(self):
-        if self._model is None or self._target is None or self._features is None:
-            return None
-        return dict(
-            id=self._id,
-            ext=self._ext,
-            compress=self._compress,
-            target=list(self._target),
-            features=list(self._features),
-            degree=self._degree,
-        )
-
-    def update_paths(self):
-        self._path = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), 'model/', self._id+self._ext)
-        self._path_poly_features = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), 'model/', self._id+'features'+self._ext)
-        self._path_meta = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), 'model/', self._id+'.json')
-
-    @property
-    def id(self):
-        return self._id
-
-    @id.setter
-    def id(self, _id):
-        self._id = _id
-        self.update_paths()
-
-    @property
-    def ext(self):
-        return self._ext
-
-    @ext.setter
-    def ext(self, _ext):
-        self._ext = _ext
-        self.update_paths()
-
-    @property
-    def compress(self):
-        return self._compress
+        return {'RMSE':rmse, 'R-Squared':r2}
 
     @property
     def target(self):
@@ -199,48 +72,44 @@ class Model():
 
 
 class Dataset():
-    def __init__(self, table_name='train', data_id='poly_data', data_ext='.joblib.gz', data_compress=4, columns=None, max_size=None, data_where=None):
-        self._id = data_id
-        self._ext = data_ext
-        self.update_paths()
-        self._compress = data_compress
+    def __init__(self, table_name='train',columns=None, max_size=None, data_where=None):
         self._max_size = max_size
         self._columns = set(columns)
         self._data = None
         self._table_name = table_name
         self._data_where = None
-        data_loaded = False
-        while not data_loaded:
-            if os.path.exists(self.path_meta):
-                old_data_meta = load(self.path_meta)
-                if set(old_data_meta['columns']) != self.columns:
-                    conflict_resolved = False
-                    while not conflict_resolved:
-                        print(
-                            "Existing dataset with conflicting columns '" + self.id + "' exists.")
-                        result_overwrite = ''
-                        while result_overwrite != 'y' and result_overwrite != 'n':
-                            result_overwrite = input(
-                                "Overwrite? (Y/n) > ").lower()
-                        if result_overwrite == 'y':
-                            os.remove(self.path_meta)
-                            os.remove(self.path)
-                        else:
-                            result_rename = ''
-                            while result_rename != 'y' and result_rename != 'n':
-                                result_rename = input(
-                                    "Give new data id? (Y/n) > ").lower()
-                            if result_rename == 'y':
-                                data_id = input("New data id: > ")
-                            else:
-                                raise Exception(
-                                    "Dataset creation failed - conflicting '"+self.id+"' already exists.")
-                else:
-                    self._data = load(self.path)
-                    data_loaded = True
-            else:
-                self.load_new_data()
-                data_loaded = True
+        # data_loaded = False
+        # while not data_loaded:
+        #     if os.path.exists(self.path_meta):
+        #         old_data_meta = load(self.path_meta)
+        #         if set(old_data_meta['columns']) != self.columns:
+        #             conflict_resolved = False
+        #             while not conflict_resolved:
+        #                 print(
+        #                     "Existing dataset with conflicting columns '" + self.id + "' exists.")
+        #                 result_overwrite = ''
+        #                 while result_overwrite != 'y' and result_overwrite != 'n':
+        #                     result_overwrite = input(
+        #                         "Overwrite? (Y/n) > ").lower()
+        #                 if result_overwrite == 'y':
+        #                     os.remove(self.path_meta)
+        #                     os.remove(self.path)
+        #                 else:
+        #                     result_rename = ''
+        #                     while result_rename != 'y' and result_rename != 'n':
+        #                         result_rename = input(
+        #                             "Give new data id? (Y/n) > ").lower()
+        #                     if result_rename == 'y':
+        #                         data_id = input("New data id: > ")
+        #                     else:
+        #                         raise Exception(
+        #                             "Dataset creation failed - conflicting '"+self.id+"' already exists.")
+        #         else:
+        #             self._data = load(self.path)
+        #             data_loaded = True
+        #     else:
+        self.load_new_data()
+        data_loaded = True
 
     def __del__(self):
         del self._data
@@ -262,66 +131,6 @@ class Dataset():
         else:
             self._data = self._data.loc[:, self._columns].dropna()
 
-    def dump(self):
-        if self.meta is None:
-            return False
-        dump(self.meta, self._path_meta)
-        dump(self._data, self._path, compress=self._compress)
-        return True
-
-    @property
-    def path(self):
-        if not self._path:
-            self.update_paths()
-        return self._path
-
-    @property
-    def path_meta(self):
-        if not self._path_meta:
-            self.update_paths()
-        return self._path_meta
-
-    @property
-    def meta(self):
-        if self._data is None:
-            return None
-        return dict(
-            table_name=self._table_name,
-            id=self.id,
-            ext=self.ext,
-            compress=self._compress,
-            columns=list(self._columns)
-        )
-        
-
-    def update_paths(self):
-        self._path = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), 'data/', self._id+self._ext)
-        self._path_meta = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), 'data/', self._id+'.json')
-
-    @property
-    def id(self):
-        return self._id
-
-    @id.setter
-    def id(self, _id):
-        self._id = _id
-        self.update_paths()
-
-    @property
-    def ext(self):
-        return self._ext
-
-    @ext.setter
-    def ext(self, _ext):
-        self._ext = _ext
-        self.update_paths()
-
-    @property
-    def compress(self):
-        return self._compress
-
     @property
     def columns(self):
         return self._columns
@@ -332,49 +141,103 @@ class Dataset():
 
 
 class PolyReg():
-    def __init__(self, max_rows=1000000, data_columns=['temp', 'dewp', 'slp', 'stp', 'visib', 'wdsp', 'max', 'min', 'altitude', 'longitude', 'latitude', 'prcp'], data_where=None,target='prcp', model_id='poly_model', model_ext='.joblib', model_compress=0, data_id='poly_data', data_ext='.joblib.gz', data_compression=4):
-        self.train_data = Dataset(data_id=data_id+"_train", data_ext=data_ext,
-                                  data_compress=data_compression, columns=data_columns, max_size=max_rows,data_where=data_where)
-        self.test_data = Dataset(table_name="test", data_id=data_id+"_test", data_ext=data_ext,
-                                 data_compress=data_compression, columns=data_columns, max_size=int(max_rows/10),data_where=data_where)
-        self.model = Model(model_id=model_id, model_ext=model_ext, model_compress=model_compress)
+    def __init__(self, model_id, model_ext='.joblib', path=None,max_rows=100000, data_columns=['mo','temp', 'dewp', 'slp', 'stp', 'visib', 'wdsp', 'max', 'min', 'altitude', 'longitude', 'latitude', 'prcp'], data_where=None,target='temp', model_compress=0):
+        self.train_data = Dataset(columns=data_columns, max_size=max_rows,data_where=data_where)
+        self.test_data = Dataset(table_name="test",columns=data_columns, max_size=int(max_rows/10),data_where=data_where)
+        self.model = None
         self.target = target
         self.train_results = None
         self.test_results = None
+        self.model_path = path
+        if self.model_path and os.path.exists(self.model_path):
+            raise Warning(f"Warning: Model already exists at: \n'\t{self.model_path}'.\n.")
 
     def train(self,degree=2):
+        if self.model is not None:
+            print("Warning: Existing model will be overwritten.")
+            response = ''
+            while response != 'y' and response != 'n':
+                response = input("Continue? (Y/n) > ").lower()
+            if response == 'n':
+                return
+        self.model = Model()
         self.train_results = self.model.train(self.target, self.train_data, degree=degree)
-        print(self.train_results)
 
     def test(self):
         self.test_results = self.model.test(self.test_data)
-        print(self.test_results)
 
     def results(self):
-        if self.train_results is not None:
-            print("Train Results:")
-            print(self.format_results(self.train_results))
-        if self.test_results is not None:
-            print("Test Results:")
-            print(self.format_results(self.test_results))
+        print("Train Results:")
+        print(self.format_results(self.train_results))
+        print("Test Results:")
+        print(self.format_results(self.test_results))
         
     def format_results(self,results):
-        return "\tRMSE: {}\n\tR-Squared: {}\n".format(results['rmse'],results['r2'])
+        if results is None:
+            return '    No Results.\n'
+        return reduce(lambda s1,s2: s1+s2,["    {0}: {1}\n".format(kv[0],kv[1]) for kv in results.items()])
+    
+    @classmethod
+    def generate_model_path(cls, id, ext='.joblib', dirpath=os.path.dirname(os.path.abspath(__file__))):
+        return os.path.join(dirpath, 'model/', id+ext)
+
+    @classmethod
+    def load_model_from_id(cls,id,ext='.joblib',dir=None):
+        if dir is None:
+            path = cls.generate_model_path(id) 
+        else:
+            path = os.path.join(dir, id+ext)
+        if os.path.exists(path):
+            pm = load(path)
+            pm.model_path = path
+            return pm
+        else:
+            return None
+    
+    @classmethod
+    def load_model_from_path(cls,path):
+        if os.path.exists(path):
+            pm = load(path)
+            pm.model_path = path
+            return pm
+        else:
+            return None
 
     def save(self):
-        self.train_data.dump()
-        print("Train data saved successfully.")
-        self.test_data.dump()
-        print("Test data saved successfully.")
-        self.model.dump()
-        print("Model saved successfully.")
+        if self.model_path is None:
+            self.save_as(self.model_path)
+        else:
+            dump(self, self.model_path)
+    
+    def save_as(self,path):
+        print("Save as...")
+        if path is not None:
+            self.model_path = path
+            if not os.path.exists(self.model_path): 
+                self.save()
+                return
+            print("Warning: Existing file will be overwritten.")
+            response = ''
+            while response != 'y' and response != 'n':
+                response = input("Continue? (Y/n) > ").lower()
+            if response == 'y':
+                os.remove(path)
+                self.save()
+        else:
+            while True:
+                self.model_path = easygui.filesavebox(msg='Save as..',title='Save PolyReg Instance',default="*.joblib",filetypes=["*.joblib"])
+                if self.model_path is None: return
+                if not os.path.exists(self.model_path): break
+            self.save()
 
-
+    
 if __name__ == '__main__':
-    data_filter = None
-    pm = PolyReg(max_rows=100000, model_id='poly_model_uk_d2_100000',
-                 data_id='poly_data_uk_d2_100000', data_compression=0, data_ext='.joblib', data_where=data_filter)
-    pm.train(degree=2)
+    pm = PolyReg.load_model_from_id('test_model')
+    if pm is None:
+        pm = PolyReg('test_model',max_rows=None)
+    pm.train()
     pm.test()
     pm.results()
     pm.save()
+    pm.save_as(PolyReg.generate_model_path('test_model'))
+
